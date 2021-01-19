43f9333e5ec86989ba12102620fc92f890b5c700
Antoine Pierson <antoine.pierson@edu.ece.fr>
2021-01-19 22:37:59 +0100

    rectif .txt + edit cvs + traitement d'un dossier

--- Main.py
+++ Main.py
@@ -4,8 +4,43 @@ from PIL.TiffTags import TAGS
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import math
+import csv
+import pandas as pd
+import glob
+import os.path
+from pip._vendor.distlib.compat import raw_input
+import path
 
 ##### Fonctions utiles #####
+
+def listdirectory(path, typeOS):
+    nameFichier = os.listdir(path)
+    baseName = []
+    for i in range(len(nameFichier)):
+        baseName.append(os.path.basename(nameFichier[i]))
+    baseName2 = []
+    for line in baseName:
+        tmp = line.split(".")
+        baseName2.append(tmp[0])
+
+    txt = ''
+    if typeOS == "windows":
+        path = path.replace('\'', '\\')
+        txt = '\\*'
+    elif typeOS == "mac":
+        path = path.replace('/', '//')
+        txt = '//*'
+    else: print("Veulliez réessayer")
+
+    fichier=[]
+    l = glob.glob(path+txt)
+    print(l)
+    for i in l:
+        if os.path.isdir(i): fichier.extend(listdirectory(i))
+        else: fichier.append(i)
+
+    return fichier, baseName2
+
 
 def Var (list, debut, N):
     var = []
@@ -18,22 +53,6 @@ def Var (list, debut, N):
         p.append(k)
     varI = np.array([np.array(p), np.array(var)])
 
-    """
-    while isRun:
-        sFi = 0
-        for k in range(debut, debut + N):
-            sFi += listF[k]
-        sFi *= 1 / N
-
-        for i in range(debut, debut + N):
-            _somme += math.sqrt(listF[i + N] - sFi)
-        _somme *= 1/N
-        var.extend(_somme)
-
-        debut += N
-        if debut > len(listF):
-            isRun = False
-    """
     return varI
 
 
@@ -52,13 +71,13 @@ def Seuil (curveRetour, alpha, N, debut):
 
     return seuil
 
+"""
+def IsRupture(Fr, Fi):
 
-#def IsRupture(Fr, Fi):
 
 
-
-   # return rupturePts
-
+   return rupturePts
+"""
 
 def Derivee (x, y):
 
@@ -71,138 +90,164 @@ def Derivee (x, y):
     plt.show()
 
     return yp
+
 ########################
 
-f = open('lot courbes de forces/1.txt', 'r')
-# content = f.readlines()
-x = []
-y = []
-# print(content[0][0])
-for line in f:
-    line = line.split()
-    x.append(float(line[0]))
-    y.append(float(line[1]))
-f.close()
-x = np.array(x)
-y = np.array(y)
-ptsArray = np.array([x, y])
-# print(ptsArray[0][0], " ", ptsArray[1][0])
+typeOS = raw_input("Etes vous sous windows ou mac ? (écrivez en minuscule)\n")
+path = raw_input("Entrez le path du dossier\n")
+fichiers, nameOutput = listdirectory(path, typeOS)
 
-plt.figure(figsize=(19.2, 10.8))
-plt.subplot(1, 2, 1)
-plt.plot(ptsArray[0], ptsArray[1], "blue")
-plt.ylabel("F (nN)")
-plt.xlabel("Z (piezo)")
-plt.title("Initial Force Curve")
-plt.grid()
+increment = 0
+while increment <= len(fichiers):
 
-##### correction piezo #####
+    ##### Lecture du fichier txt #####
 
-tmp = 0
-a = 0
-r = 400
-for i in range(r):
-    tmp += ptsArray[1][i]
-a = tmp / r
-for i in range(ptsArray.shape[1]):
-    ptsArray[1][i] -= a
+    f = open(fichiers[increment], 'r')
+    x = []
+    y = []
+    for line in f:
+        tmp = line.split()
+        x.append(float(tmp[0]))
+        y.append(float(tmp[1]))
+    f.close()
+    x = np.array(x)
+    y = np.array(y)
+    ptsArray = np.array([x, y])
+    '''
+    plt.figure(figsize=(19.2, 10.8))
+    plt.subplot(1, 2, 1)
+    plt.plot(ptsArray[0], ptsArray[1], "blue")
+    plt.ylabel("F (nN)")
+    plt.xlabel("Z (piezo)")
+    plt.title("Initial Force Curve")
+    plt.grid()
+    '''
+    ##### correction piezo #####
 
-plt.subplot(1, 2, 2)
-plt.plot(ptsArray[0], ptsArray[1], "blue")
-plt.ylabel("F (nN)")
-plt.xlabel("d (nm)")
-plt.title("Force Curve corrected piezo")
-plt.grid()
-plt.show()
+    tmp = 0
+    a = 0
+    r = 400
+    for i in range(r):
+        tmp += ptsArray[1][i]
+    a = tmp / r
+    for i in range(ptsArray.shape[1]):
+        ptsArray[1][i] -= a
+    '''
+    plt.subplot(1, 2, 2)
+    plt.plot(ptsArray[0], ptsArray[1], "blue")
+    plt.ylabel("F (nN)")
+    plt.xlabel("Def")
+    plt.title("Force Curve corrected piezo")
+    plt.grid()
+    plt.show()
+    '''
+    ##### calcul variance et seuil pour trouver points de rupture #####
 
-##### calcul variance et seuil pour trouver points de rupture #####
+    maxY = max(ptsArray[1])
+    debut = 0
+    for i in range(ptsArray.shape[1]):
+        if ptsArray[1][i] == maxY:
+            debut = i
 
-maxY = max(ptsArray[1])
-debut = 0
-for i in range(ptsArray.shape[1]):
-    if ptsArray[1][i] == maxY:
-        debut = i
+    curveRetourX = []
+    curveRetourY = []
+    for i in range(debut, ptsArray.shape[1], 1):
+        curveRetourX.append(ptsArray[0][i])
+        curveRetourY.append(ptsArray[1][i])
+    curveRetour = np.array([curveRetourX, curveRetourY])
 
-curveRetourX = []
-curveRetourY = []
-for i in range(debut, ptsArray.shape[1], 1):
-    curveRetourX.append(ptsArray[0][i])
-    curveRetourY.append(ptsArray[1][i])
-curveRetour = np.array([curveRetourX, curveRetourY])
+    N = 10
+    ##### Variance
 
-N = 10
-##### Variance
+    varI = Var(list(curveRetour[1]), 0, N)
+    '''
+    plt.figure(figsize=(19.2, 10.8))
+    plt.ylabel("Var(i)")
+    plt.xlabel("i")
+    plt.title("----")
+    plt.grid()
+    plt.plot(varI[0], varI[1], "blue")
+    plt.show()
+    # yp = Derivee(curveRetour[0], curveRetour[1])
+    '''
+    ##### Seuil
+    alpha = 20
+    #alpha = float(input("Choose the alpha treshold\n"))
+    seuil = Seuil(curveRetour, alpha, N, debut)
+    tabSeuil = []
+    for i in range(varI.shape[1]):
+        tabSeuil.append(seuil)
+    '''
+    plt.figure(figsize=(19.2, 10.8))
+    plt.plot(varI[0], varI[1], color="blue", label="Var(i)")
+    plt.plot(list(varI[0]), tabSeuil, color="orange", label="seuil")
+    plt.ylabel("Var(i)")
+    plt.xlabel("i")
+    plt.title("----")
+    plt.legend()
+    plt.grid()
+    plt.show()
+    '''
+    ##### Pics de Rupture et conditions
 
-varI = Var(list(curveRetour[1]), 0, N)
+    Fi = np.array(curveRetourY[1])
+    FrX = []
+    FrY = []
+    for i in range(1, varI.shape[1] - 1, 1):
+        if varI[1][i] > seuil and varI[1][i - 1] < varI[1][i] and varI[1][i + 1] < varI[1][i]:
+            FrY.append(varI[1][i])
+            FrX.append(varI[0][i])
+    Fr = np.array([FrX, FrY])
+    '''
+    plt.figure(figsize=(19.2, 10.8))
+    plt.plot(varI[0], varI[1], color="blue", label="Var(i)")
+    plt.plot(list(varI[0]), tabSeuil, color="orange", label="seuil")
+    plt.scatter(Fr[0], Fr[1], color="red", label="pics")
+    plt.ylabel("Var(i)")
+    plt.xlabel("i")
+    plt.title("----")
+    plt.legend()
+    plt.grid()
+    plt.show()
+    '''
+    """
+    rupturePts = IsRupture(Fr, Fi)
+    """
 
-plt.figure(figsize=(19.2, 10.8))
-plt.ylabel("Var(i)")
-plt.xlabel("i")
-plt.title("----")
-plt.grid()
-plt.plot(varI[0], varI[1], "blue")
-plt.show()
-#yp = Derivee(curveRetour[0], curveRetour[1])
+    ##### Cherche Force d'adhésion avec position #####
 
-##### Seuil
+    posFrForceCurveX = []
+    posFrForceCurveY = []
+    for i in range(curveRetour.shape[1]):
+        for j in range(Fr.shape[1]):
+            if i == Fr[0][j]:
+                posFrForceCurveX.append(curveRetour[0][i])
+                posFrForceCurveY.append(curveRetour[1][i])
+    posFrForceCurve = np.array([posFrForceCurveX, posFrForceCurveY])
+    '''
+    plt.figure(figsize=(19.2, 10.8))
+    plt.plot(ptsArray[0], ptsArray[1], color="blue", label="Force Curve")
+    plt.scatter(posFrForceCurve[0], posFrForceCurve[1], color="red", label="pics")
+    plt.ylabel("F (nN)")
+    plt.xlabel("Def")
+    plt.title("Force Curve")
+    plt.legend()
+    plt.grid()
+    plt.show()
+    '''
+    ##### module écriture données au format csv #####
 
-#listPtsX = list(ptsArray[0])
-alpha = float(input("Choose the alpha treshold\n"))
-seuil = Seuil(curveRetour, alpha, N, debut)
-tabSeuil = []
-for i in range(varI.shape[1]):
-    tabSeuil.append(seuil)
-print(seuil)
-plt.figure(figsize=(19.2, 10.8))
-plt.plot(varI[0], varI[1], color="blue", label="Var(i)")
-plt.plot(list(varI[0]), tabSeuil, color="orange", label="seuil")
-plt.ylabel("Var(i)")
-plt.xlabel("i")
-plt.title("----")
-plt.legend()
-plt.grid()
-plt.show()
+    zippedList = list(zip(posFrForceCurveX, posFrForceCurveY))
+    # print(zippedList)
+    dataFrame = pd.DataFrame(zippedList, columns=['Def', 'Force '])
+    if typeOS == 'windows':
+        dataFrame.to_csv(path + "\'" + nameOutput[increment] + '.csv', sep=';')
+    elif typeOS == 'mac':
+        dataFrame.to_csv(path + '/' + nameOutput[increment] + '.csv', sep=';')
 
-##### Pics de Rupture et conditions
+    # df = pd.read_csv('test.csv', delimiter=';')
+    # print(df)
 
-Fi = np.array(curveRetourY[1])
-FrX = []
-FrY = []
-for i in range(1, varI.shape[1]-1, 1):
-    if varI[1][i] > seuil and varI[1][i-1] < varI[1][i] and varI[1][i+1] < varI[1][i]:
-        FrY.append(varI[1][i])
-        FrX.append(varI[0][i])
-Fr = np.array([FrX, FrY])
-plt.figure(figsize=(19.2, 10.8))
-plt.plot(varI[0], varI[1], color="blue", label="Var(i)")
-plt.plot(list(varI[0]), tabSeuil, color="orange", label="seuil")
-plt.scatter(Fr[0], Fr[1], color="red", label="pics")
-plt.ylabel("Var(i)")
-plt.xlabel("i")
-plt.title("----")
-plt.legend()
-plt.grid()
-plt.show()
-
-#rupturePts = IsRupture(Fr, Fi)
+    increment += 1
 
 
-##### calcul Force d'adhésion avec position #####
-
-posFrForceCurveX = []
-posFrForceCurveY = []
-for i in range(curveRetour.shape[1]):
-    for j in range(Fr.shape[1]):
-        if i == Fr[0][j]:
-            posFrForceCurveX.append(curveRetour[0][i])
-            posFrForceCurveY.append(curveRetour[1][i])
-posFrForceCurve = np.array([posFrForceCurveX, posFrForceCurveY])
-plt.figure(figsize=(19.2, 10.8))
-plt.plot(ptsArray[0], ptsArray[1], color="blue", label="Force Curve")
-plt.scatter(posFrForceCurve[0], posFrForceCurve[1], color="red", label="pics")
-plt.ylabel("d (nm")
-plt.xlabel("F (nN)")
-plt.title("Force Curve")
-plt.legend()
-plt.grid()
-plt.show()
